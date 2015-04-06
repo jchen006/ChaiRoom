d41dd4d266b375d99d314f31fd436a3dbd57118d
Jeffrey Chen <jeffreyhc@berkeley.edu>
2015-04-06 13:31:07 -0700

    Added seat sensors and removed main.xml

---
+++ chaiRoom-Sensors/simulator/seat_sensor.js
@@ -0,0 +1,41 @@
+// KPR Script file
+
+var THEME = require ("themes/flat/theme");
+var CONTROL = require ("mobile/control");
+var PinsSimulators = require ("PinsSimulators");
+
+exports.pins = {
+	seats: {type: "I2C", address: 0x48}
+};
+
+exports.configure = function() {
+	this.pinsSimulator = shell.delegate("addSimulatorPart", {
+		header : { 
+			label : "Seats", 
+			name : "Seat Sensor", 
+			iconVariant : PinsSimulators.SENSOR_MODULE
+		},
+		axes : [
+			new PinsSimulators.AnalogInputAxisDescription(
+				{
+					valueLabel : "seat",
+					valueID : "seats",
+					defaultControl : PinsSimulators.SLIDER,
+					minValue : 0,
+					maxValue : 30,
+					value : 0
+				}
+			),
+		]
+	});
+};
+
+exports.close = function() {
+	shell.delegate("removeSimulatorPart", this.pinsSimulator);
+};
+
+exports.read = function() {
+	var axes = this.pinsSimulator.delegate("getValue");
+	trace("Temp: " + axes.temperature);
+	return axes.temperature;				
+};
