3632986ed14bdd0713ace7d91e7fd1f664a8b3d0
Kamel <kdabwan@berkeley.edu>
2015-04-08 14:50:16 -0700

    started app impl

--- chaiRoom-Application/src/main.js
+++ chaiRoom-Application/src/main.js
@@ -1,1 +1,59 @@
 // KPR Script file
+var THEME = require('themes/sample/theme');
+var SCREEN = require('mobile/screen');
+var CONTROL = require('mobile/control');
+var MODEL = require('mobile/model');
+var SCROLLER = require('mobile/scroller');
+var TOOL = require('mobile/tool');
+
+var applicationIconTexture = new Texture('assets/icon.png', 1) ;
+var applicationIconSkin = new Skin({ texture: applicationIconTexture, width: 80, height: 80, aspect: 'fit', });
+var blackSkin = new Skin({ fill: '#7C7C7C',});
+
+var whiteSkin = new Skin({ fill: '#30A8BE',});
+
+
+
+Handler.bind("/main", Object.create(MODEL.ScreenBehavior.prototype, {
+	hasSelection: { value: function(data, delta) {
+				var selection = data.selection + delta;
+				return (0 <= selection) && (selection < data.items.length)
+			}},
+			getSelection: { value: function(data, delta) {
+				data.selection += delta;
+				return data.items[data.selection];
+			}},
+			onDescribe: { value: function(query, selection) {
+				var title = "ChaiRoom";
+				return {
+					Screen: MainScreen,
+					title: title,
+					items: null,
+					more: false,
+					path: model.root,
+					scroll: {
+						x: 0,
+						y: 0
+					},
+					selection: -1,
+				}
+			}}
+}));
+
+var Header = SCREEN.EmptyHeader.template(function($) { return { skin: blackSkin, contents: [
+	TOOL.BackButton($, { }),
+	TOOL.HeaderTitle($, { style: THEME.plainHeaderTitleStyle, }),
+], }});
+var MainScreen = SCREEN.EmptyScreen.template(function($) { return { contents: [
+	SCREEN.EmptyBody($, { skin: whiteSkin, anchor: 'BODY', contents: [
+		SCROLLER.VerticalScroller($, { contents: [
+			Column($, { left: 0, right: 0, top: 0, anchor: 'LIST', }),
+			SCROLLER.VerticalScrollbar($, { }),
+			SCROLLER.TopScrollerShadow($, { }),
+			SCROLLER.BottomScrollerShadow($, { }),
+		], }),
+	], }),
+	Header($, { anchor: 'HEADER', }),
+], }});
+
+var model = application.behavior = new MODEL.ApplicationBehavior(application);
