560f75c8f2da4505d0bd3e639f9b436817946448
Kamel <kdabwan@berkeley.edu>
2015-04-08 15:43:06 -0700

    new layout

--- chaiRoom-Application/src/main.js
+++ chaiRoom-Application/src/main.js
@@ -8,52 +8,93 @@ var TOOL = require('mobile/tool');
 
 var applicationIconTexture = new Texture('assets/icon.png', 1) ;
 var applicationIconSkin = new Skin({ texture: applicationIconTexture, width: 80, height: 80, aspect: 'fit', });
-var blackSkin = new Skin({ fill: '#7C7C7C',});
+var tabTexture = (screenScale == 2) ? new Texture('assets/tabs.png', 1) : (screenScale == 1.5) ? new Texture('assets/tabs.png', 1) : new Texture('assets/tabs.png', 1);
+var tabSkin = new CONTROL.Skin(tabTexture, THEME.tabDisabledEffect, THEME.tabEnabledEffect, THEME.tabSelectedEffect);
+var blackSkin = new Skin({ fill: '#30A8BE',});
 
 var whiteSkin = new Skin({ fill: '#30A8BE',});
 
-
-
+var Body = SCREEN.EmptyBody.template(function($) { return { skin: blackSkin, }});
 Handler.bind("/main", Object.create(MODEL.ScreenBehavior.prototype, {
-	hasSelection: { value: function(data, delta) {
-				var selection = data.selection + delta;
-				return (0 <= selection) && (selection < data.items.length)
-			}},
-			getSelection: { value: function(data, delta) {
-				data.selection += delta;
-				return data.items[data.selection];
-			}},
-			onDescribe: { value: function(query, selection) {
-				var title = "ChaiRoom";
-				return {
-					Screen: MainScreen,
-					title: title,
-					items: null,
-					more: false,
-					path: model.root,
-					scroll: {
-						x: 0,
-						y: 0
-					},
-					selection: -1,
-				}
-			}}
+	hasSelection: { value: 
+		function(data, delta) {
+			var tab = data.tabs[data.selection];
+				var selection = tab.selection + delta;
+				return (0 <= selection) && (selection < tab.items.length)
+		},
+	},
+	getSelection: { value:
+		function(data, delta) {
+			var tab = data.tabs[data.selection];
+				var selection = tab.selection + delta;
+				return tab.items[selection];
+		},
+	},
+	onDescribe: { value: 
+		function(query, selection) {
+					return {
+						Screen: MainScreen,
+						selection: 0,
+						skin: tabSkin,
+						tabs: [
+							{
+								Header: Header,
+								Pane: HomePane,
+								items: null,
+								more: false,
+								scroll: {x: 0, y:0},
+								selection: -1,
+								variant: 2,
+								title: "Photos",
+							},
+							{
+								Header: Header,
+								Pane: HomePane,
+								items: null,
+								more: false,
+								scroll: {x: 0, y:0},
+								selection: -1,
+								variant: 1,
+								title: "Videos",
+							},
+							{
+								Header: Header,
+								Pane: HomePane,
+								items: null,
+								more: false,
+								scroll: {x: 0, y:0},
+								selection: -1,
+								variant: 0,
+								title: "Songs",
+							},
+						]
+					};
+		},
+	},
 }));
-
 var Header = SCREEN.EmptyHeader.template(function($) { return { skin: blackSkin, contents: [
 	TOOL.BackButton($, { }),
 	TOOL.HeaderTitle($, { style: THEME.plainHeaderTitleStyle, }),
 ], }});
+
 var MainScreen = SCREEN.EmptyScreen.template(function($) { return { contents: [
-	SCREEN.EmptyBody($, { skin: whiteSkin, anchor: 'BODY', contents: [
-		SCROLLER.VerticalScroller($, { contents: [
-			Column($, { left: 0, right: 0, top: 0, anchor: 'LIST', }),
-			SCROLLER.VerticalScrollbar($, { }),
-			SCROLLER.TopScrollerShadow($, { }),
-			SCROLLER.BottomScrollerShadow($, { }),
-		], }),
+	$.tabs[$.selection].Pane($.tabs[$.selection], { anchor: 'BODY', }),
+	SCREEN.TabFooter($, { anchor: 'FOOTER', }),
+	$.tabs[$.selection].Header($.tabs[$.selection], { anchor: 'HEADER', }),
+], }});
+
+var HomePane = Body.template(function($) { return { contents: [
+	SCROLLER.VerticalScroller($, { contents: [
+		Column($, { left: 0, right: 0, top: 0, anchor: 'LIST' }),
+		SCROLLER.VerticalScrollbar($, { }),
+	], }),
+], }});
+
+var HomeScreen = SCREEN.EmptyScreen.template(function($) { return { contents: [
+	Body($, { anchor: 'BODY', contents: [
+		Media($, { left: 0, right: 0, top: 0, bottom: 0,  url: $.url, }),
 	], }),
 	Header($, { anchor: 'HEADER', }),
 ], }});
 
-var model = application.behavior = new MODEL.ApplicationBehavior(application);
+application.behavior = new MODEL.ApplicationBehavior(application);
